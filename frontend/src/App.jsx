fd38904b369696a110bf718eb4656ba06a23df88
Pranshuverma9696 <er.pranshuverma68@gmail.com>
2026-01-14 13:34:42 +0530

    Inital commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import Hello from "./Hello";
+import Visit from "./Visit";
+import Welcome from "./Welcome";
+
+function App() {
+  return (
+
+    <>
+    <h1>Welcome to my Profile</h1>
+      <Hello />
+      <Welcome />
+      <Visit />
+      <Welcome />
+     
+      <Visit />
+       <Welcome />
+      <Hello />
+    </>
+  );
+}
+
+export default App;
