6e0ea951c2ebe8d83672ca3548bb914c0f2681e4
pranshuverma9696 <er.pranshuverma68@gmail.com>
2026-04-16 15:54:57 +0530

    this is my component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,37 @@
-import Hello from "./Hello";
-import Visit from "./Visit";
-import Welcome from "./Welcome";
+
+import Users from "./componets/ax";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import {BrowserRouter,Route,Routes ,Link} from "react-router-dom";
+
 
 function App() {
   return (
-
     <>
-    <h1>Welcome to my Profile</h1>
-      <Hello />
-      <Welcome />
-      <Visit />
-      <Welcome />
-     
-      <Visit />
-       <Welcome />
-      <Hello />
+    
+{/* <div>
+<nav>
+  <Link to="/" >Home </Link>
+  <Link to="/about" >about </Link>
+  <Link to="/contact" >contact </Link>
+
+</nav>
+
+<Routes>
+<Route path = '/' element = {<Home />}/>
+<Route path = '/about' element = {<About/>}/>
+<Route path = '/contact' element = {<Contact/>}/>
+
+</Routes>
+ 
+
+
+
+</div> */}
+<Users/>
+
     </>
   );
 }
-
 export default App;
