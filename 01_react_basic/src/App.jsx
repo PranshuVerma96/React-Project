38fa87e582a670a1b74913f463620285d2fd1f7e
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-05-05 15:41:49 +0530

    Add files via upload
    This is my first react app for learning basic difference using create react app bundler and using vite bundler and clearliy the concept of how to component rendering between there.

---
+++ 01_react_basic/src/App.jsx
@@ -0,0 +1,11 @@
+import Chai from "./Chai"
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React || Pranshu and we used of this  how to react project created . </h1>
+<Chai />
+    </div>
+  )
+}
+
+export default App
